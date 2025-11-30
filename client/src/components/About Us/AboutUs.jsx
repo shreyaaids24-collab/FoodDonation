4e11703f3c3d2347f6e142805f071f0b048ecfff
shreyaaids24-collab <shreya.aids24@cmrit.ac.in>
2025-11-30 16:14:21 +0530

    first commit

---
+++ client/src/components/About Us/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutus">
+      <div className="info-text">
+        <h1 className="info-heading">Donate Food</h1>
+        <p className="info-subheading">
+          we connect hearts through meals. Our mission is to fight hunger by redistributing surplus food to those who need it most. 
+          Together, let’s build a hunger-free India  one meal, one act of kindness at a time.
+        </p>
+      </div>
+      <div className="info-img"></div>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ client/src/components/Our Visison/Vision.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Vision.css";
+
+const Vision = () => {
+  return (
+    <div className="vision">
+      <div className="vision-img"></div>
+      <div className="vision-text">
+        <h1 className="info-heading">Our Vision</h1>
+        <p className="vision-subheading">
+          Our vision is to create a hunger-free India 
+          where every individual has access to nutritious meals, and no food goes to waste. 
+          We strive to build a compassionate community where generosity and social 
+          responsibility are part of everyday life. 
+          By connecting surplus food with those in need, we aim to transform lives, 
+          restore dignity, and foster a culture of sharing and kindness across the nation.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Vision;
